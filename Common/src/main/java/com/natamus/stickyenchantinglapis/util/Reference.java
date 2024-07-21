151fd360e43a84135e3fc0ced85475b81cc025c7
Rick South <rick@natamus.com>
2024-07-21 19:40:24 +0200

    (v1.1) Updated mod icon.

--- Common/src/main/java/com/natamus/stickyenchantinglapis/util/Reference.java
+++ Common/src/main/java/com/natamus/stickyenchantinglapis/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.stickyenchantinglapis.util;
 public class Reference {
 	public static final String MOD_ID = "stickyenchantinglapis";
 	public static final String NAME = "Sticky Enchanting Lapis";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
