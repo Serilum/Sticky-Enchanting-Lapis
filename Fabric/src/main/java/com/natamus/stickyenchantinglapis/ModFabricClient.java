aacce3e17c5d8e2aa64fb79039acfb95a8d7a914
Rick South <rick@natamus.com>
2024-07-28 07:13:12 +0200

    (v1.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/stickyenchantinglapis/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/stickyenchantinglapis/ModFabricClient.java
@@ -1,10 +1,16 @@
 package com.natamus.stickyenchantinglapis;
 
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.stickyenchantinglapis.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		ModCommon.registerPackets();
 
 		registerEvents();
